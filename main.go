158ddc1ac5abfa5a1f18bcb31ffcfc0c6b97c5eb
spongebob <vdmntrn@gmail.com>
2026-09-26 00:54:49 +0500

    test: query and print login + balance

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
-	_ "github.com/jackc/pgx/v5"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 type Account struct {
@@ -12,5 +16,28 @@ type Account struct {
 }
 
 func main() {
+	// urlExample := "postgres://username:password@localhost:5432/database_name"
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres:1234@localhost:5432/testdb")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed connecting to database: %v\n", err)
+		os.Exit(1)
+	}
 
+	defer conn.Close(context.Background())
+
+	var login string
+	var balance float64
+	sql_query := "select login, balance from users"
+
+	// Gets rows and stores them in rows var
+	rows, _ := conn.Query(context.Background(), sql_query)
+	// For each row in rows var, scan results into login and balance, then run func()
+	_, err = pgx.ForEachRow(rows, []any{&login, &balance}, func() error {
+		fmt.Fprintf(os.Stdout, "%s %f\n", login, balance)
+		return nil
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ForEachRow failed: %v\n", err)
+		os.Exit(1)
+	}
 }
